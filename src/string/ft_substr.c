68be6af7a8862bda57f84f3aedce1bc34774b53b
zettai-dev <zettai.dev@gmail.com>
2019-10-30 15:06:44 +0100

    Fixed substring

--- src/string/ft_substr.c
+++ src/string/ft_substr.c
@@ -6,19 +6,25 @@
 /*   By: aouahib <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 17:18:56 by aouahib           #+#    #+#             */
-/*   Updated: 2019/10/29 14:02:32 by aouahib          ###   ########.fr       */
+/*   Updated: 2019/10/30 15:06:27 by aouahib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(const char *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
+	size_t	slen;
+	size_t	max_len;
 
 	if (!s)
 		return (0);
-	if (start > ft_strlen(s))
+	slen = ft_strlen(s);
+	max_len = slen - start + 1;
+	if (max_len < len)
+		len = max_len;
+	if (start > slen)
 		return (ft_strdup(""));
 	sub = malloc(len + 1);
 	if (sub)
